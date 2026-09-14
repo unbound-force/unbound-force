6cbe6d81ead8718a58f38fd1dffc6a0dd8734b94
Jay Flowers <jay.flowers@gmail.com>
2026-09-14 11:14:36 -0400

    fix: satisfy staticcheck De Morgan check

--- internal/sandbox/config.go
+++ internal/sandbox/config.go
@@ -260,7 +260,7 @@ func stripJSONComments(data []byte) []byte {
 		// Block comment.
 		if i+1 < len(src) && src[i] == '/' && src[i+1] == '*' {
 			i += 2
-			for i+1 < len(src) && !(src[i] == '*' && src[i+1] == '/') {
+			for i+1 < len(src) && (src[i] != '*' || src[i+1] != '/') {
 				i++
 			}
 			if i+1 < len(src) {
